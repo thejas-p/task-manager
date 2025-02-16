934ce77f179d456e60ca87010f5d2af3613eff9e
thejas <pthejaz@gmail.com>
2025-02-16 23:52:27 +0530

    added dto

---
+++ src/main/java/com/tp/taskmanager/task_manager/dto/TaskDTO.java
@@ -0,0 +1,16 @@
+package com.tp.taskmanager.task_manager.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private UserDTO user;
+
+}
---
+++ src/main/java/com/tp/taskmanager/task_manager/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.tp.taskmanager.task_manager.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String username;
+
+
+}
